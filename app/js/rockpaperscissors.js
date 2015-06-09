f05fac2fa3057a12de2f993cfdac8bdd7c07d879
Jon Harlan <jonharlan@Jons-MacBook-Pro.local>
2015-06-09 15:25:54 -0700

    committing rps conditionals

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -43,6 +43,27 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+	
+	if (playerMove == computerMove) {
+		winner = 'tie';
+		
+	} else if (playerMove == 'rock' && computerMove == 'scissors') {
+		winner = 'player';
+	} else if (playerMove == 'paper' && computerMove == 'rock') {
+		winner = 'player';
+	} else if (playerMove == 'scissors' && computerMove == 'paper') {
+		winner = 'player';
+		
+	} else if (computerMove == 'rock' && PlayerMove == 'scissors') {
+		winner = 'computer';
+	} else if (computerMove == 'paper' && PlayerMove == 'rock') {
+		winner = 'computer';
+	} else if (computerMove == 'scissors' && PlayerMove == 'paper') {
+		winner = 'computer';
+	}
+
+	
+	
     return winner;
 }
 
