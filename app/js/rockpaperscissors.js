213961d7b7923d79a645fbdea76f49f61ab7825f
Jon Harlan <jonharlan@Jons-MacBook-Pro.local>
2015-06-21 23:42:08 -0700

    play2Fivegit add .

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -67,8 +67,28 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+	var playerMove = randomPlay();
+	var computerMove = randomPlay();
+	
+	
+	
+	while (playerWins < 5 && computerWins < 5) {
+		var winner = getWinner(playerMove,computerMove);
+		if (winner == 'tie'){
+		    null;
+		}
+		else if (winner === 'player'){
+			playerWins += 1;
+		}
+		else if (winner === 'computer'){
+			computerWins += 1;
+		}
+		console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+		console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+		
+		playerMove = randomPlay();
+		computerMove = randomPlay();
+	}
+	return [playerWins, computerWins];
 }
 
